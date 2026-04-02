ca302874d3d399cf2e8451c48019eb026586ca57
williammaina <wmaina8442@gmail.com>
2026-04-02 11:21:01 +0300

    added functions to add a new burger and drink

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+let burgers =["Hambuger", "cheeseburger"];
+let featuredDrink = 'StrawberryMilkshake';
+function addBurger ()
+{
+    let newBurger ='flatburger'
+    burgers.push(newBurger)
+let anotherNewBurger ="bacon burger"
+burgers.push (anotherNewBurger)
+}
+addBurger()
+console.log(burgers)
+
+const changeFeaturedDrink =() => {
+    featuredDrink= "The JavaShake"
+}
+changeFeaturedDrink()
+console.log( featuredDrink)
